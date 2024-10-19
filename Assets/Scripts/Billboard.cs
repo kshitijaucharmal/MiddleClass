c8be8a8450766b103cc3a56274dedf23e505566e
KshitijAucharmal <kshitijaucharmal21@gmail.com>
2024-10-19 02:43:36 -0700

    All Dialogues Setup

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -16,6 +16,6 @@ public class Billboard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(-_camera.position);
+        transform.LookAt(transform.position + _camera.forward);
     }
 }
